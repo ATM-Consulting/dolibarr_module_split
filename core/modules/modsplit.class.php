9d5e8e0a26b68711a3da63944d8d3cf5120243e6
florian HENRY <florian.henry@atm-consuliting.fr>
2020-08-13 08:48:15 +0200

    fix hook call

--- core/modules/modsplit.class.php
+++ core/modules/modsplit.class.php
@@ -95,7 +95,7 @@ class modsplit extends DolibarrModules
             // Set this to relative path of css if module has its own css file
             //'css' => '/split/css/mycss.css.php',
             // Set here all hooks context managed by module
-            'hooks' => array('propalcard', 'operationordercard')
+            'hooks' => array('propalcard', 'invoicecard', 'ordercard', 'operationordercard')
             // Set here all workflow context managed by module
             //'workflow' => array('order' => array('WORKFLOW_ORDER_AUTOCREATE_INVOICE'))
         );
